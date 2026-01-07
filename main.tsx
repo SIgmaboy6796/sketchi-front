b6ebe547a7d88ddbb555714263ed6733db68edc2
SIgmaboy6796 <ohioemail7@gmail.com>
2026-01-07 10:03:26 +0000

    file path fix ?

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../ui/App'
+import App from './ui/App'
 import '../ui/ui.css'
 
 ReactDOM.createRoot(document.getElementById('app')!).render(
